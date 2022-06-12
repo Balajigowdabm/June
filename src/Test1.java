3ce9d2717fb0901af106f8b667605f639cde06db
Indium Software <balajimgowda17@gmail.com>
2022-06-12 11:13:06 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Balaji";
+if(name=="Balaji") {
+	System.out.println("He is the one");
+}
+else {
+	System.out.println("He is a fraud");
+}
+	}
+
+}
